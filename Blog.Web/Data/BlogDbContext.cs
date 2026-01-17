4f1ef9ae644e96f4e731f147780800c66cb64bce
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2026-01-17 08:27:07 +0530

    Add project files.

---
+++ Blog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Tag> Tags { get; set; }        
+    }
+}
---
+++ Blog.Web/Migrations/20260114024450_initial_load.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_load : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
