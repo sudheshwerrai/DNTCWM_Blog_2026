97354976ae2573e90487ab8c476b6199977dde60
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2026-02-23 11:52:58 +0530

    Get All Blogposts

---
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Web.Repositories.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly IBlogPostRepository _blogPostRepository = null;
+
+        public AdminBlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var blogPosts = await _blogPostRepository.GetAllAsync();
+            return View(blogPosts);
+        }
+    }
+}
---
+++ Blog.Web/Migrations/20260223053935_addBlogPost.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBlogPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BlogPostId",
+                table: "Tags",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Heading = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeaturedImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlHandle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visible = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPosts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_BlogPostId",
+                table: "Tags",
+                column: "BlogPostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_BlogPosts_BlogPostId",
+                table: "Tags",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_BlogPosts_BlogPostId",
+                table: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "BlogPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_BlogPostId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "BlogPostId",
+                table: "Tags");
+        }
+    }
+}
