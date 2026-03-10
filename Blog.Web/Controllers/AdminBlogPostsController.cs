a64b822d57eb8334b3af89552af8782dbf08b409
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2026-03-10 12:34:13 +0530

    Update & Delete BlogPost

--- Blog.Web/Controllers/AdminBlogPostsController.cs
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -39,7 +39,6 @@ namespace Blog.Web.Controllers
             return View(blogPost);
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(BlogPostRequest blogPostRequest)
@@ -65,17 +64,80 @@ namespace Blog.Web.Controllers
                 {
                     var selectedTag = Guid.Parse(selectedTagId);
                     var existingTag = await _tagRepository.GetAsync(selectedTag);
-                    if (existingTag != null) 
+                    if (existingTag != null)
                     {
                         listOfTags.Add(existingTag);
                     }
                 }
 
-                blogPost.Tags=listOfTags;
+                blogPost.Tags = listOfTags;
                 await _blogPostRepository.AddAsync(blogPost);
                 return RedirectToAction(nameof(Index));
             }
             return BadRequest();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var blogPostFromDb = await _blogPostRepository.GetAsync(id);
+            var tags = await _tagRepository.GetAllAsync();
+
+            var blogPost = new BlogPostRequest();
+            blogPost.BlogPost = blogPostFromDb;
+            blogPost.Tags = tags.Select(t => new SelectListItem { Text = t.Name, Value = t.Id.ToString() });
+            blogPost.SelectedTags = blogPostFromDb.Tags.Select(t => t.Id.ToString()).ToArray();
+
+            return View(blogPost);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, BlogPostRequest blogPostRequest)
+        {
+            if (ModelState.IsValid && blogPostRequest.BlogPost.Id == id)
+            {
+                var blogPost = new BlogPost
+                {
+                    Id= blogPostRequest.BlogPost.Id,
+                    Heading = blogPostRequest.BlogPost.Heading,
+                    PageTitle = blogPostRequest.BlogPost.PageTitle,
+                    Content = blogPostRequest.BlogPost.Content,
+                    ShortDescription = blogPostRequest.BlogPost.ShortDescription,
+                    FeaturedImageUrl = blogPostRequest.BlogPost.FeaturedImageUrl,
+                    UrlHandle = blogPostRequest.BlogPost.UrlHandle,
+                    PublishedDate = blogPostRequest.BlogPost.PublishedDate,
+                    Author = blogPostRequest.BlogPost.Author,
+                    Visible = blogPostRequest.BlogPost.Visible,
+                };
+
+                var listOfTags = new List<Tag>();
+
+                foreach (var selectedTagId in blogPostRequest.SelectedTags)
+                {
+                    var selectedTag = Guid.Parse(selectedTagId);
+                    var existingTag = await _tagRepository.GetAsync(selectedTag);
+                    if (existingTag != null)
+                    {
+                        listOfTags.Add(existingTag);
+                    }
+                }
+
+                blogPost.Tags = listOfTags;
+
+                await _blogPostRepository.UpdateAsync(blogPost);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<RedirectToActionResult> Delete(Guid id)
+        {
+            await _blogPostRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
 
     }
