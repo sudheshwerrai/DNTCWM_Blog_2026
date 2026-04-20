22937ea2c23634577a5ddc626f2ddf40b368a060
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2026-04-20 13:19:22 +0530

    Shown BlogPost on Home Page

--- Blog.Web/Controllers/HomeController.cs
+++ Blog.Web/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
-using System.Diagnostics;
 using Blog.Web.Models;
+using Blog.Web.Repositories.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Blog.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlogPostRepository _blogPostRepository = null;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            _blogPostRepository = blogPostRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var blogPosts = await _blogPostRepository.GetAllAsync();
+            return View(blogPosts);
         }
 
         public IActionResult Privacy()
