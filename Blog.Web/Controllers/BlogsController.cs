d1a1c7f11474c6d93dc435b1cb2f4b10c41b2ed2
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2026-04-21 11:37:57 +0530

    BlogPost Detail Page integrated

---
+++ Blog.Web/Controllers/BlogsController.cs
@@ -0,0 +1,43 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly IBlogPostRepository _blogPostRepository = null;
+
+        public BlogsController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+        [HttpGet]
+        [ActionName("BlogPostDetail")]
+        public async Task<IActionResult> Index(string urlHandle)
+        {
+            var blogPost = await _blogPostRepository.GetBlogByUrlHanlder(urlHandle);
+
+            BlogDetailsViewModel blogPostViewModel = null;
+
+            if (blogPost != null) 
+            {
+                blogPostViewModel = new BlogDetailsViewModel
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    ShortDescription = blogPost.ShortDescription,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    PublishedDate = blogPost.PublishedDate,
+                    Author = blogPost.Author,
+                    Visible = blogPost.Visible,
+                    Tags = blogPost.Tags
+                };
+            }
+            return View("BlogPostDetail", blogPostViewModel);
+        }
+    }
+}
---
+++ Blog.Web/Models/ViewModels/BlogDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Models.ViewModels
+{
+    public class BlogDetailsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Heading { get; set; }
+        
+        public string PageTitle { get; set; }
+
+        public string Content { get; set; }
+        
+        public string ShortDescription { get; set; }
+       
+        public string FeaturedImageUrl { get; set; }
+       
+        public string UrlHandle { get; set; }
+      
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+        public string Author { get; set; }
+
+        public bool Visible { get; set; }
+        
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
